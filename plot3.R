aa101214b26aa514f85ee0490a4ac07b1d277706
MattDSquared <matthew.d.davis@gmail.com>
2015-04-12 11:47:52 -0700

    plot3

---
+++ plot3.R
@@ -0,0 +1,85 @@
+# plot3.R loads data and creates the third plot described in README.md
+
+## =========== download, load, and clean data =========== 
+
+# load data if it hasn't already been loaded
+filename <- "household_power_consumption.zip"
+
+if (!file.exists(filename)){ 
+    message("Downloading data")
+    
+    # download binary file from https using mode wb
+    setInternet2(use = TRUE) # uses ie protocols to dl from https
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+                  destfile = filename,
+                  mode="wb")
+    
+    dateDL <- date()
+} else {
+    message("using previously downloaded file")
+}
+
+
+# load data file
+if (!exists("DT")) {
+    message("Loading data file")
+    
+    # unzip file
+    datafile <- unzip("household_power_consumption.zip")
+    
+    DT <- read.csv(datafile, 
+                   sep=";", 
+                   na.strings="?", 
+                   stringsAsFactors=FALSE)
+    
+    # all lower case is easier to type
+    names(DT) <- tolower(names(DT))
+    
+    # FIXME: if this errors, there may be problems re-running this code 
+    message("Cleaning data file")
+    # note: time zone must be specified otherwise, when daylight savings time
+    # occurs, R treats rollover as na value
+    DT$datetime <- strptime(paste(DT$date,DT$time,sep=" "), 
+                            format="%d/%m/%Y %H:%M:%S",
+                            tz="GMT")
+    if (exists("subDT")) rm(subDT)
+}else {
+    message("using previously loaded and cleaned data")
+}
+# IMPORTANT: do not modify DT below this point, 
+# unexpected behavior may occur
+
+## =========== Generate plot =========== 
+
+# extract sub data for all of days 2007-02-01 and 2007-02-02
+message("extracting dates")
+tstart <- as.POSIXct("2007-02-01 00:00:00",tz="GMT")
+tend <- as.POSIXct("2007-02-03 00:00:00",tz="GMT")
+plotidx <- (DT$datetime >= tstart) & (DT$datetime < tend)
+
+# open window
+PPI <- 96 # pixels per inch
+windows(width=480/PPI, height=480/PPI, xpinch=PPI, ypinch=PPI)
+devid <- dev.cur()
+with(subset(DT, plotidx), 
+     plot(datetime, sub_metering_1, 
+          type="l",
+          col="black", 
+          xlab="",
+          ylab="Energy sub metering"))
+
+with(subset(DT, plotidx), 
+     points(datetime, sub_metering_2, type="l", col="red"))
+
+with(subset(DT, plotidx), 
+     points(datetime, sub_metering_3, type="l", col="blue"))
+
+legend("topright",
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       cex=.7,
+       col=c("black","red","blue"),
+       lty=1)
+
+# save plot
+dev.copy(png, file="plot3.png")
+dev.off()
